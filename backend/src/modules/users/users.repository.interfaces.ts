2d1f079931f9939e60f1721fbba20164a396f2fd
Elias Neto <eliasneto072@gmail.com>
2026-03-01 08:30:04 -0300

    add interface repository

--- backend/src/modules/users/users.repository.interfaces.ts
+++ backend/src/modules/users/users.repository.interfaces.ts
@@ -1,17 +1,11 @@
-import { IUser,IUserPublic } from "./users.types";
-import { CreateUserDTO, UpdateUserDTO } from "./users.dto";
-
+import { IUser, IUserPublic } from './users.types';
+import { CreateUserData, UpdateUserData } from './users.repository.types';
 
 export interface IUserRepository {
-
-    findAll(): Promise<IUserPublic[]>
-    findById(id: string): Promise<IUserPublic | null>
-
-    // para login (precisa do password)
-    findByEmail(email: string): Promise<IUser | null>;
-
-    create(data: CreateUserDTO): Promise<IUserPublic>
-    update(id: string, data: UpdateUserDTO): Promise<IUserPublic | null>
-    delete(id: string): Promise<void>
-
- }
+  findAll(): Promise<IUserPublic[]>;
+  findById(id: string): Promise<IUserPublic | null>;
+  findByEmail(email: string): Promise<IUser | null>;
+  create(data: CreateUserData): Promise<IUserPublic>;
+  update(id: string, data: UpdateUserData): Promise<IUserPublic>;
+  delete(id: string): Promise<void>;
+}
