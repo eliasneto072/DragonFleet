f556178340fe9148ee32db8633fc71a2e6ab4f82
Caio Sousa <caiosousaap@gmail.com>
2026-04-10 19:17:49 -0300

    refactor: app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import { router } from './routes/routes';
 import { errorMiddleware } from './middlewares/error.middleware';
 
 const app = express();
+
+app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(express.json());
 
 app.use(express.json());
 
