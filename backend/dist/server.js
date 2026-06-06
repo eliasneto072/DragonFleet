352c0b8a36865521ee8cdfa1237587a1ce6c4dd3
Elias Neto <eliasneto072@gmail.com>
2026-06-06 01:46:51 -0300

    chore: remove dist from version control

--- backend/dist/server.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = require("./app");
-const env_1 = require("./config/env");
-const PORT = env_1.env.PORT ?? 3000;
-app_1.app.listen(PORT, () => {
-    console.log(`🚀 Server running on port ${PORT}`);
-});
